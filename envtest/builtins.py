4600caf46496144af6c7606907da8d3b837f2a82
aeb23 <aimee.burton23@imperial.ac.uk>
2023-10-09 14:49:26 +0100

    Add the file

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,11 @@
 import numpy as np
+from scipy import ndimage
 
-
-__all__ = ['rand_array']
+__all__ = ['rand_array','smooth_image']
 
 
 def rand_array(shape):
     return np.random.rand(*shape)
+
+def smooth_image(a, sigma=1):
+    return ndimage.gaussian_filter(a, sigma=sigma)
